7f9ea34dc975a9cf31f4b5ebc17e9bb674a0e17a
albfue <97479943+albfue@users.noreply.github.com>
2023-04-24 13:27:05 +0200

    Update WP2_OpenGuise_Erw.js
    Kommafehler entfernt

--- data_includes/WP2_OpenGuise_Erw.js
+++ data_includes/WP2_OpenGuise_Erw.js
@@ -516,7 +516,7 @@ PennController("Meta1",
                .center()
                .print()
                , 
-               ,
+             
                //Alter
                newDropDown("age", "Bitte eine Option ausw&auml;hlen")
                .settings.add("18" , "19" , "20", "21" , "22" , "23", "24" , "25" , "26", "27" , "28" , "29", "30" , "31", "32","33", "34" , "35", "36","37","38","39","über 40")
