2ab065fef24d7663547c55bd2cd84bcadeb4f526
albfue <97479943+albfue@users.noreply.github.com>
2023-04-24 13:04:21 +0200

    Update WP2_OpenGuise_Erw.js
    Dropdown hinzugefügt: frühere Experimentteilnahme

--- data_includes/WP2_OpenGuise_Erw.js
+++ data_includes/WP2_OpenGuise_Erw.js
@@ -485,6 +485,34 @@ PennController("Meta1",
                //.settings.center()
                .print()
                ,
+                              newTextInput("beforeinput")
+               .settings.size(150,40)
+               .log()
+               .settings.hidden()
+               ,
+               newText("before_input", "")
+               .settings.after(getTextInput("beforeinput"))
+               ,
+               newText("before", "Haben Sie vorher schon an einem Experiment dieser Art teilgenommen? (Zeitungstext korrigieren, Audioaufnahmen hören")
+               ,
+               newDropDown("before",  "<br>" +"Bitte eine Option ausw&auml;hlen")
+               .settings.add("Ja", "Nein")
+               .log()
+               .settings.after(getText("before_input"))
+               .settings.callback(
+                   getDropDown("before")
+                   .test.selected("Ja")
+                   .success(getTextInput("beforeinput").settings.visible()
+                )
+               ,
+               newCanvas("before", 1000, 40)
+               .settings.add(0, 0, getText("before"))
+               .settings.add(500,3, getDropDown("before"))
+               //.settings.center()
+               .print()
+               .wait()
+               
+               ,
                //Alter
                newDropDown("age", "Bitte eine Option ausw&auml;hlen")
                .settings.add("18" , "19" , "20", "21" , "22" , "23", "24" , "25" , "26", "27" , "28" , "29", "30" , "31", "32","33", "34" , "35", "36","37","38","39","über 40")
